ad1325cbfe9ec8fe708c82b847d24512c2a905c9
oo <oo@xif.at>
2019-12-09 15:17:05 +0900

    feat: add i18n-related settings

--- config/application.example.rb
+++ config/application.example.rb
@@ -12,6 +12,22 @@ module Appname
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    # ActiveRecord のデフォルトのバリデーションエラーの文言などを
+    # 利用できるようにするため、 available_locals に en を追加し、 fallbacks も
+    # en にする。
+    # 殆どのデフォルトの文言は rails-i18n によって日本語化されているはずだが、
+    # rails-i18n に抜けや漏れがある場合もあるので、一応設定しておく。
+    config.i18n.available_locales = %i[en ja]
+    config.i18n.fallbacks = %i[en]
+
+    # 言語を日本語に設定する。
+    # 対応する言語を増やす場合に available_locales に言語を追加し、
+    # https://guides.rubyonrails.org/v6.0/i18n.html#managing-the-locale-across-requests
+    # を参考に言語を切り替える仕組みを別途導入する。
+    # 切り替える仕組みを導入しない限りは、 default_locale で設定した言語が
+    # 利用される。
+    config.i18n.default_locale = :ja
+
     # SMTP の設定。ただし実際に SMTP を使うかどうかは
     # config.action_mailer.delivery_method 次第。
     # config.action_mailer.delivery_method はここで設定してもいいし、
